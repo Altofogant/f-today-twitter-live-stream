b28eb2475872650da7a055c5fd793e701349ee00
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-19 18:21:57 +0300

    got the tweet data stream working

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const config = require('dotenv').config();
 const TOKEN = process.env.TWITTER_BEARER_TOKEN;
 
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream&expansions=author_id';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
-const rules = [{ value: 'giveaway' }];
+const rules = [{ value: 'fuck' }];
 
 //Get stream rules
 async function getRules() {
@@ -15,8 +15,7 @@ async function getRules() {
         }
     })
 
-    console.log(response.body);
-    return response.body;
+    return response.body
 }
 
 //Set stream rules
@@ -56,7 +55,24 @@ async function deleteRules(rules) {
         }
     })
 
-    return response.body;
+    return response.body
+}
+
+function streamTweets() {
+    const stream = needle.get(streamURL, {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`
+        }
+    })
+
+    stream.on('data', (data) => {
+        try {
+            const json = JSON.parse(data)
+            console.log(json)
+        } catch (error) {}
+    })
+
+    return stream;
 }
 
 (async () => {
@@ -64,15 +80,17 @@ async function deleteRules(rules) {
 
     try {
         //Get all stream Rules
-        currentRules = await getRules();
+        currentRules = await getRules()
 
         //Delete all stream rules
-        await deleteRules(currentRules);
+        await deleteRules(currentRules)
 
         //Set rules based on rules array
-        await setRules();
+        await setRules()
     } catch (error) {
-        console.log(error);
-        process.exit(1);
+        console.log(error)
+        process.exit(1)
     }
+
+    streamTweets()
 })()
